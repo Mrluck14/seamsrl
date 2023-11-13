6256a08d5f71bec142f49ebaab3e319cfec0a6b9
Mrluck14 <110512314+Mrluck14@users.noreply.github.com>
2023-11-13 09:29:49 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('#slides').superslides();
+
+$('.owl-carousel').owlCarousel({
+	items: 1,
+	loop: true,
+    margin:10,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    smartSpeed:1000
+});
